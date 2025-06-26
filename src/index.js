9ce4d2b50e8084ff00ce8bd89501bf26f58aad92
Katrin <katrinivanovaa@yahoo.com>
2025-06-26 20:26:07 +0100

    added poem dymnamically
    styled dynamically --> added typerwriter effect and changed colour dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let colorChangeElement = document.querySelector("#example-poem");
+
+  colorChangeElement.style.color = "#001774cf";
+
+  new Typewriter("#example-poem", {
+    loop: false,
+  })
+    .typeString(
+      "Appreciation for experience: sight, smell, sound, taste, touch."
+    )
+    .start();
+}
+
+let poemGeneratorForm = document.querySelector("#form-1");
+poemGeneratorForm.addEventListener("submit", generatePoem);
